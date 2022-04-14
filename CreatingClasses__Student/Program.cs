d647116bd51fe7336b3c0fd033a3e03d34534091
Egor <Egor@ASUS-EGORM>
2022-04-14 21:09:36 +0300

    Добавьте файлы проекта.

---
+++ CreatingClasses__Student/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CreatingStudent
+{
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+    }
+    internal class Program
+    {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+
+            student.id = Guid.NewGuid();
+            student.firstName = "Egor";
+            student.lastName = "Medvedev";
+            student.middleName = "Petrovich";
+            student.age = 23;
+            student.group = "MMM-20";
+
+            return student;
+        }
+        static void Print(Student student)
+        {
+            Console.WriteLine($"ФИО: {student.lastName} {student.firstName} {student.middleName}");
+            Console.WriteLine($"Возраст: {student.age} years");
+            Console.WriteLine($"Группа: {student.group}");
+            Console.WriteLine($"id: {student.id}");
+
+        }
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            Print(firstStudent);
+        }
+    }
+}
